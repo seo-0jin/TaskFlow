9d98d1a9ba226ccd920d51736e89df7862efd66d
epik35 <epik35@forcs.com>
2025-12-03 11:40:57 +0900

    FE - 회원가입 페이지 대략적인 구조 및 router 추가

--- Home/src/router/AppRouter.tsx
+++ Home/src/router/AppRouter.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 // import DashboardPage from "../pages/DashboardPage";
 import { useAuth } from "../hooks/useAuth";
+import { RoutePath } from "../const/RoutePath";
+import SignUpPage from "../pages/SignUpPage";
 
 export default function AppRouter() {
   const { user, loading } = useAuth();
@@ -15,14 +17,21 @@ export default function AppRouter() {
     <BrowserRouter>
       <Routes>
         {/* 로그인 페이지 */}
-        <Route path="/login" element={user ? <Navigate to="/dashboard" replace /> : <LoginPage />} />
+        <Route
+          path={RoutePath.LOGIN}
+          element={user ? <Navigate to={RoutePath.DASHBOARD} replace /> : <LoginPage />}
+        />
+        {/* 회원가입 페이지 */}
+        <Route
+          path={RoutePath.SIGNUP}
+          element={user ? <Navigate to={RoutePath.LOGIN} replace /> : <SignUpPage />}
+        />
 
         {/* 보호된 페이지 (로그인 필요) */}
         {/* <Route
           path="/dashboard"
           element={user ? <DashboardPage /> : <Navigate to="/login" replace />}
         /> */}
-
         {/* 기본 루트 처리 */}
         <Route
           path="*"
