aa91fbb332ac7451fa50c38d99564521fb163527
epik35 <epik35@forcs.com>
2026-02-13 09:05:58 +0900

    FE - Footer > github 링크 연결

--- Home/src/layout/FooterLayout.tsx
+++ Home/src/layout/FooterLayout.tsx
@@ -1,7 +1,16 @@
 import styles from '@/scss/layout.module.scss';
 
 const FooterLayout = () => {
-  return <footer className={styles.footer}>Copyright © IDEN YOUNGJIN SEO All rights reserved.</footer>;
+  return <footer className={styles.footer}>
+    <div>
+      Copyright © IDEN YOUNGJIN SEO All rights reserved.
+    </div>
+    <div>
+      <a className={styles.github_icon} href='https://github.com/seo-0jin' target='_blank'>
+        <img src='/public/icons/git-hub.png' />
+      </a>
+    </div>
+  </footer>;
 };
 
 export default FooterLayout;
